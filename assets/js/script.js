0c8d36591584f44261d14458b88c422b9e0b42ff
wildarte <wildarte10@gmail.com>
2023-09-01 15:23:25 -0300

    criacao page quem somos

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,48 @@ btn_menu_mobile.addEventListener('click', () => {
 btn_close_menu.addEventListener('click', () => {
     nav_menu.classList.remove('open_menu')
 })
+
+
+//====== carousel ========================================================
+$(document).ready(function(){
+    $(".carousel_top").owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        dots: false,
+        autoplay:true,
+        autoplayTimeout:200000,
+        autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:3
+            }
+        }
+    });
+
+    const items_carousel = document.querySelectorAll('.carousel_top article');
+
+    console.log(items_carousel.length);
+
+    items_carousel.forEach((item, index) => {
+
+        if((index % 2) == 0){
+
+            item.style.width = '80%';
+            item.style.margin = 'auto';
+
+            item.querySelector('.info_inner_card_carousel').classList.add('position_right');
+        }else{
+            item.querySelector('.info_inner_card_carousel').classList.remove('position_right');
+
+        }
+
+    });
+});
+//====== carousel ========================================================
