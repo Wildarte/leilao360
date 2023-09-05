2dab1718d0a014fb16cd1bbfef9190617ca5b144
wildarte <wildarte10@gmail.com>
2023-09-05 18:52:10 -0300

    page login & cadastro

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,18 @@ const btn_menu_mobile = document.querySelector('.btn_menu_mobile');
 const btn_close_menu = document.querySelector('.btn_close_menu');
 const nav_menu = document.querySelector('nav.menu');
 
-btn_menu_mobile.addEventListener('click', () => {
-    nav_menu.classList.add('open_menu')
-})
+if(btn_menu_mobile){
+    btn_menu_mobile.addEventListener('click', () => {
+        nav_menu.classList.add('open_menu')
+    })
+}
 
-btn_close_menu.addEventListener('click', () => {
-    nav_menu.classList.remove('open_menu')
-})
+if(btn_close_menu){
+    btn_close_menu.addEventListener('click', () => {
+        nav_menu.classList.remove('open_menu')
+    })
+}
+
 
 
 //====== carousel ========================================================
@@ -23,10 +28,12 @@ $(document).ready(function(){
         autoplayHoverPause:true,
         responsive:{
             0:{
-                items:1
+                items:1,
+                nav: false
             },
             600:{
-                items:1
+                items:2,
+                nav: false
             },
             1000:{
                 items:3
@@ -38,19 +45,79 @@ $(document).ready(function(){
 
     console.log(items_carousel.length);
 
-    items_carousel.forEach((item, index) => {
+    if(window.matchMedia('(max-width: 1000px)').matches){
 
-        if((index % 2)!= 0){
+    }else{
+        items_carousel.forEach((item, index) => {
 
-            item.style.width = '80%';
-            item.style.margin = 'auto';
+            if((index % 2)!= 0){
+    
+                item.style.width = '80%';
+                item.style.margin = 'auto';
+    
+                item.querySelector('.info_inner_card_carousel').classList.add('position_right');
+            }else{
+                item.querySelector('.info_inner_card_carousel').classList.remove('position_right');
+    
+            }
+    
+        });
+    }
 
-            item.querySelector('.info_inner_card_carousel').classList.add('position_right');
+    window.addEventListener('resize', () => {
+        
+        if(window.matchMedia('(max-width: 1000px)').matches){
+
+            items_carousel.forEach((item) => {
+
+                item.style.width = "auto"
+
+            });
+
         }else{
-            item.querySelector('.info_inner_card_carousel').classList.remove('position_right');
-
+            items_carousel.forEach((item, index) => {
+    
+                if((index % 2)!= 0){
+        
+                    item.style.width = '80%';
+                    item.style.margin = 'auto';
+        
+                    item.querySelector('.info_inner_card_carousel').classList.add('position_right');
+                }else{
+                    item.querySelector('.info_inner_card_carousel').classList.remove('position_right');
+        
+                }
+        
+            });
         }
+    })
 
-    });
+    
 });
 //====== carousel ========================================================
+
+
+
+//========================= page login ====================================
+const show_pass = document.getElementById('show_pass');
+const field_switch_pass = document.querySelector('.field_switch_pass');
+
+show_pass.addEventListener('click', () => {
+
+    if(field_switch_pass.getAttribute('type') == 'password'){
+        field_switch_pass.setAttribute('type', 'text');
+        show_pass.querySelector('i').classList.replace('bi-eye-fill','bi-eye-slash-fill')
+    }else{
+        field_switch_pass.setAttribute('type', 'password');
+        show_pass.querySelector('i').classList.replace('bi-eye-slash-fill','bi-eye-fill')
+    }
+
+});
+//========================= page login ====================================
+
+
+const form_element_cad = document.getElementById('form_element_cad');
+if(form_element_cad){
+    form_element_cad.getBoundingClientRect().height;
+}
+
